7e8e0416e9108166708b523bdde5cf6ddf01d732
Sergio Orihuela <eFirewall@gmail.com>
2018-05-07 20:10:49 +0200

    Programa 2 hecho

---
+++ Python_Learning/Python Jumpstart/02-guess-number-app/you_try/game.py
@@ -0,0 +1,23 @@
+import random
+
+print('-------------------------------------------')
+print('            ADIVINA EL NUMERO')
+print('-------------------------------------------')
+print()
+
+numero = random.randint(0, 100)
+nombre = input('¿Como te llamas?')
+adivina = -1
+
+
+while adivina != numero:
+    adivina = input('Adivina que numero estoy pensando de 0 a 100?')
+    adivina = int(adivina)
+    if adivina > numero:
+        print('{}, el número {} es demasiado alto'.format(nombre, adivina))
+    elif adivina < numero:
+        print('{}, el número {} es demasiado bajo'.format(nombre, adivina))
+    else:
+        print('¡Enhorabuena {}, {} es el numero!'.format(nombre, adivina))
+
+print('')
