5c0733a18d50afecf3be03e3d5a564d7c2cba056
Sergio Orihuela <eFirewall@gmail.com>
2018-05-13 21:34:27 +0200

    Finished with the journal app...

--- Python_Learning/Python Jumpstart/Diario/journal.py
+++ Python_Learning/Python Jumpstart/Diario/journal.py
@@ -1,8 +1,20 @@
+"""This is the journal module."""
 import os
 
+
 def load(name):
+    """Create and load a new journal.
+
+    Args:
+        variable (name):  This base name of the journal to load.
+
+    Returns:
+        data: A new journal data structure populated with the file data.
+
+    """
     data = []
     filename = get_full_pathname(name)
+
     if os.path.exists(filename):
         with open(filename) as fin:
             for entry in fin.readlines():
@@ -12,6 +24,7 @@ def load(name):
 
 
 def save(name, journal_data):
+    """Save the journal data."""
     filename = get_full_pathname(name)
     print("..... saving to: {}".format(filename))
 
@@ -21,9 +34,11 @@ def save(name, journal_data):
 
 
 def get_full_pathname(name):
+    """Collect the path to the file."""
     filename = os.path.abspath(os.path.join('./journals/' + name + '.jrl'))
     return filename
 
 
 def add_entry(text, journal_data):
+    """Add entry."""
     journal_data.append(text)
--- Python_Learning/Python Jumpstart/Diario/program.py
+++ Python_Learning/Python Jumpstart/Diario/program.py
@@ -1,11 +1,14 @@
+"""Program module."""
 import journal
 
 def main():
+    """Main function to call them all."""
     print_header()
     run_event_loop()
 
 
 def print_header():
+    """Print the header."""
     print('-------------------------------')
     print('         JOURNAL APP')
     print('-------------------------------')
@@ -13,7 +16,7 @@ def print_header():
 
 
 def run_event_loop():
-
+    """Event looper."""
     print('What do you wanna do with your journal?')
     cmd = 'EMPTY'
     journal_name = 'default'
@@ -28,12 +31,14 @@ def run_event_loop():
         elif cmd == 'a':
             add_entry(journal_data)
         elif cmd != 'x' and cmd:
-            print("Sorry, we cannot understand '{}'.".format(cmd))
+            print("Sorr y, we cannot understand '{}'.".format(cmd))
 
     print('That will be all. Good bye!')
     journal.save(journal_name, journal_data)
 
+
 def list_entries(data):
+    """List entries."""
     print('Your journal entries: ')
     entries = reversed(data)
     for idx, entry in enumerate(entries):
@@ -41,9 +46,11 @@ def list_entries(data):
 
 
 def add_entry(data):
+    """Add entry."""
     text = input('Type your enry, <enter>, to exit: ')
     journal.add_entry(text, data)
     # data.append(text)
 
 
-main()
+if __name__ == '__main__':
+    main()
