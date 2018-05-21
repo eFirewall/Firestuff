4827e844670e36ba753227d7dc4b414892dd4aa8
Sergio Orihuela <sorihuela@atsistemas.com>
2018-05-21 17:26:38 +0200

    Limpieza

--- Python_Learning/Python Jumpstart/Parser/parser.py
+++ Python_Learning/Python Jumpstart/Parser/parser.py
@@ -11,7 +11,7 @@ def main():
 
 def mostrar_header():
     print('---------------------------------------')
-    print('             PARSER 1.0')
+    print('              PARSER 1.0')
     print('---------------------------------------')
     print('')
 
