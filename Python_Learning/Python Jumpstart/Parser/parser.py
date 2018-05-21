6c9b2b8e9d6825e8886e25bfaac5a8b9368f328d
Sergio Orihuela <eFirewall@gmail.com>
2018-05-21 20:07:14 +0200

    Añadidas fechas al parseator 1.0

--- Python_Learning/Python Jumpstart/Parser/parser.py
+++ Python_Learning/Python Jumpstart/Parser/parser.py
@@ -1,17 +1,23 @@
+"""Parseator 1.0."""
+
 import json
+import io
+import datetime
 
 
 def main():
+    """Orquestra todo el script."""
     mostrar_header()
     mostrar_test_plan()
-    read_file()
+    looper()
     # TODO Limitar el numero de iteraciones
     # TODO guardar a un fichero externo nuevo
-
+    # TODO Control de ficheros. Si el fichero no existe que arroje error p.ej.
 
 def mostrar_header():
+    """Imprime el header."""
     print('---------------------------------------')
-    print('              PARSER 1.0')
+    print('           PARSEATOR 1.0')
     print('---------------------------------------')
     print('')
     print('')
@@ -20,20 +26,29 @@ def mostrar_header():
 
 
 def mostrar_test_plan():
-    with open('lastrun.json') as data_file:
+    """Imprime, al principio del fichero, el nombre del test plan."""
+    with io.open('lastrun.json', 'r', encoding='utf8') as data_file:
         data_item = json.load(data_file)
         plan = data_item['name']
+        fecha = datetime.datetime.now()
+        hoy = fecha.date().strftime("%d-%m-%Y")
     print('------------------------------------')
+    print('')
+    print('Fecha de reporte: *{}*'.format(hoy))
+    print('')
     print('*Test plan: *')
     print('*{}*'.format(plan))
     print('------------------------------------')
+    print('')
+    print('')
 
 
-def read_file():
-    with open('lastrun.json') as data_file:
+def looper():
+    """Hace un loop y va metiendo cosas en el reporte."""
+    with io.open('lastrun.json', 'r', encoding='utf8') as data_file:
         data_item = json.load(data_file)
     counter = int(66)
-
+    """El loop en cuestion."""
     for i in range(0, counter + 1):
         passfail = (data_item['results'][i]['tests']['status']).upper()
         codigo = data_item['results'][i]['responseCode']['code']
