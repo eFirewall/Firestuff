02a979d588d1f5c9cc9fde2e86198dd38cb56a0b
Sergio Orihuela <sorihuela@atsistemas.com>
2018-05-21 17:36:09 +0200

    Arreglos varios

--- Python_Learning/Python Jumpstart/Parser/parser.py
+++ Python_Learning/Python Jumpstart/Parser/parser.py
@@ -14,18 +14,18 @@ def mostrar_header():
     print('              PARSER 1.0')
     print('---------------------------------------')
     print('')
+    print('')
+    print('')
+    print('')
 
 
 def mostrar_test_plan():
     with open('lastrun.json') as data_file:
         data_item = json.load(data_file)
+        plan = data_item['name']
     print('------------------------------------')
-    print('')
-    print('')
     print('*Test plan: *')
-    plan = data_item['name']
-    print('*'+plan+'*')
-    print('')
+    print('*{}*'.format(plan))
     print('------------------------------------')
 
 
@@ -33,28 +33,33 @@ def read_file():
     with open('lastrun.json') as data_file:
         data_item = json.load(data_file)
     counter = int(66)
+
     for i in range(0, counter + 1):
+        passfail = (data_item['results'][i]['tests']['status']).upper()
         codigo = data_item['results'][i]['responseCode']['code']
         mensaje = data_item['results'][i]['responseCode']['name']
+        caso = data_item['results'][i]['name']
+        url = data_item['results'][i]['url']
+        resp = ('El codigo de respuesta es: *{} {}*'.format(codigo, mensaje))
+
         print('------------------------------------')
         print('')
         print('')
         print('*Caso de Prueba:*')
-        print(data_item['results'][i]['name'])
+        print(caso)
         print('')
         print('*Peticion:*')
-        print(data_item['results'][i]['url'])
+        print(url)
         print('')
-        print('Resultado esperado:')
+        print('*Resultado esperado:*')
         print('')
         print('')
-        print('Resultado obtenido:')
-        print('El codigo de respuesta es: {} {}'.format(codigo, mensaje))
+        print('*Resultado obtenido:*')
+        print(resp)
         print('')
         print('')
         print('*Valoracion:*')
-        passfail = (data_item['results'][i]['tests']['status']).upper()
-        print('*' + passfail + '*')
+        print('*{}*'.format(passfail))
         print('')
         print('*Evidencia:*')
         print('{code}')
