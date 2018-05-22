071a1dc565aa48d5ed2b4ca1a7e62c600fcf1e93
Sergio Orihuela <sorihuela@atsistemas.com>
2018-05-22 10:37:44 +0200

    Terminada funcionalidad basica de parseator a consola... ahora qe escriba en un fichero...

--- Python_Learning/Python Jumpstart/Parser/parser.py
+++ Python_Learning/Python Jumpstart/Parser/parser.py
@@ -3,16 +3,17 @@
 import json
 import io
 import datetime
+import os
+import sys
 
 
 def main():
     """Orquestra todo el script."""
     mostrar_header()
+    Comprueba_fichero()
     mostrar_test_plan()
     looper()
-    # TODO Limitar el numero de iteraciones
     # TODO guardar a un fichero externo nuevo
-    # TODO Control de ficheros. Si el fichero no existe que arroje error p.ej.
 
 
 def mostrar_header():
@@ -26,6 +27,20 @@ def mostrar_header():
     print('')
 
 
+def Comprueba_fichero():
+    print('Comprobaciones iniciales:')
+    print('')
+    filename = ('lastrun.json')
+    if os.path.exists(filename):
+        print('El fichero "lastrun.json" existe.')
+    else:
+        print('El fichero "lastrun.json" NO existe')
+        print('')
+        print('No es posible continuar... OH POR DIOS!:')
+        print('')
+        sys.exit('ERROR CRITICO: NADA ES COMO DEBE!!!')
+
+
 def mostrar_test_plan():
     """Imprime, al principio del fichero, el nombre del test plan."""
     with io.open('lastrun.json', 'r', encoding='utf8') as data_file:
@@ -33,35 +48,36 @@ def mostrar_test_plan():
         plan = data_item['name']
         fecha = datetime.datetime.now()
         hoy = fecha.date().strftime("%d-%m-%Y")
+        counter = len(data_item["results"])
     print('------------------------------------')
-    print('')
-    print('Fecha de reporte: *{}*'.format(hoy))
     print('')
     print('*Test plan: *')
     print('*{}*'.format(plan))
+    print('')
+    print('Numero de casos: *{}*'.format(counter))
+    print('')
+    print('Fecha de reporte: *{}*'.format(hoy))
+    print('')
     print('------------------------------------')
-    print('')
-    print('')
 
 
 def looper():
     """Hace un loop y va metiendo cosas en el reporte."""
     with io.open('lastrun.json', 'r', encoding='utf8') as data_file:
         data_item = json.load(data_file)
-    counter = int(66)
     """El loop en cuestion."""
-    for i in range(0, counter + 1):
+    counter = len(data_item["results"])
+    for i in range(counter):
         passfail = (data_item['results'][i]['tests']['status']).upper()
         codigo = data_item['results'][i]['responseCode']['code']
         mensaje = data_item['results'][i]['responseCode']['name']
         caso = data_item['results'][i]['name']
         url = data_item['results'][i]['url']
         resp = ('El codigo de respuesta es: *{} {}*'.format(codigo, mensaje))
-
         print('------------------------------------')
         print('')
         print('')
-        print('*Caso de Prueba:*')
+        print('*Caso de Prueba: *')
         print(caso)
         print('')
         print('*Peticion:*')
